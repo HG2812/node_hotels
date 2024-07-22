37b85331d89847d81c512c38ad9e93bb65d5a62a
Harsh Goyal <harsh@192.168.1.11>
2024-07-23 00:27:09 +0530

    shift mongodb to online server

--- express/db.js
+++ express/db.js
@@ -1,8 +1,9 @@
 const mongoose=require('mongoose');
+require('dotenv').config();
 
 //defining the mongodb url
-const mongoURL="mongodb://localhost:27017/mydatabase"
-
+//const mongoURL="mongodb://localhost:27017/mydatabase"
+const mongoURL=process.env.MONGODB_URL;
 //setup mongodb connection
 
 mongoose.connect(mongoURL,{
@@ -18,6 +19,5 @@ db.on('connected',()=>{
 db.on('disconnected',()=>{
     console.log("mongodb disconnected");
 })
-
 //export the datbase connection
 module.exports=db;
--- express/server.js
+++ express/server.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express();
 const db = require('./db');
+require('dotenv').config();
 const bodyParser = require('body-parser');
+const PORT=process.env.PORT || 3000;
 app.use(bodyParser.json());
 app.get('/', function (req, res) {
   res.send('Hello World')
@@ -11,6 +13,6 @@ app.use('/person', personRoutes);
 const menuRoutes = require('./routes/menuRoutes');
 app.use('/menu', menuRoutes);
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log("server is live");
 });
